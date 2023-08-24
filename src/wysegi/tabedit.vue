e11be76494eec25adc9359387895d6df504ebdee
Kyle Bateman <kyle@batemans.org>
2023-08-23 19:04:36 -0600

    Wysegi working with current libs

--- src/wysegi/tabedit.vue
+++ src/wysegi/tabedit.vue
@@ -10,7 +10,7 @@
       <wylib-dbp :state="state.windows[0].client" :autoEdit="false" :env="env" @execute="addWin"/>
     </wylib-win>
     <div class="subwindows">
-      <wylib-win v-for="win,idx in state.windows" v-if="idx > 0 && win" topLevel=true :env="env" :key="idx" :state="win" @close="close(idx)">
+      <wylib-win v-for="win,idx in state.windows" v-if="idx > 0 && win" topLevel=true :env="env" :key="idx" :state="win" @close="r=>close(idx,r)">
         <wylib-dbp :state="win.client" :env="env"/>
       </wylib-win>
     </div>
@@ -29,18 +29,23 @@ export default {
 //  data() { return {
 //  }},
   methods: {
-    addWin(row, pkey, keyVals) {
-//console.log("Add Window", row, pkey, keyVals)
-      let i, view = keyVals.slice(0,2).join('.')
-      for (i = 0; this.state.windows[i]; i++) {}
-      if (i <= 0) view = 'wm.table_pub'
-      let newWin = {posted: true, client: {dbView: view, loaded: true}}
-      this.state.windows.splice(i, 0, newWin)
-//console.log(" windows:", this.state.windows)
+    addWin(row, pkey, keyVals) {	//console.log("addWin", row, pkey, keyVals)
+      let i = this.state.windows.length
+        , dbView = (i > 0) ? keyVals.slice(0,2).join('.') : 'wm.table_pub'
+        , newWin = {posted: true, client: {dbView, loaded: true}}
+//console.log(" windows:", i, this.state.windows)
+      Wylib.Common.addWindow(this.state.windows, newWin, this)
     },
-    close(idx) {
-      this.state.windows.splice(idx,1)
+    close(idx, reopen) {
+      Wylib.Common.closeWindow(this.state.windows, idx, this, reopen)
     },
+  },
+//  beforeMount: function() {		//console.log("tabedit before mount:", this.state)
+//  },
+  mounted: function() {			console.log("tabedit mounted:", this.state)
+    this.$nextTick(() => {
+      this.state.windows[0].layer = 1	//Set this window at the back
+    })
   },
 }
 </script>
--- src/wysegi/wysegi.js
+++ src/wysegi/wysegi.js
@@ -19,22 +19,25 @@ import TabEdit from './tabedit.vue'
 import EntEdit from './entity.vue'
 import ErdView from './erd.vue'
 
-const AppTemplate = `
+const Template = `
   <wylib-app tag="wylib_wysegi" title="Wyseman GUI" :state="state" v-slot="ws" :tabs="tabs" :current="state.curTab" @tab="(t)=>{state.curTab = t}" help="Viewer for tables and views in a Wyseman managed database">
-    <keep-alive><component :is="currentComp" :state="state.tabs[state.curTab]" :env="ws.env"/></keep-alive>
+    <component v-for="t in tabs" :key="t.tag" v-show="curTab==t.tag" v-if="hasRun[t.tag]" :is="components[t.tag]" :tag="t.tag" :view="views[t.tag]" :state="state.tabs[t.tag]" :env="ws.env"/>
   </wylib-app>
 `
-new Vue(Object.assign({el: '#app', template: AppTemplate}, {
+new Vue({
+  el: '#app',
+  template: Template,
   components: {'wylib-app': Wylib.Application, 'erdViewer': ErdView, 'tabEditor': TabEdit, 'entEditor': EntEdit},
   data() { return {
     state:	{
       curTab: 'db', 
       tabs: {
-        db:	{windows: [{posted: true, client: {dbView: 'wm.table_data', loaded: true}}]},
+        db:	{windows: [{posted: true, client: {dbView: 'wm.table_pub', loaded: true}}]},
         ent:	{windows: [{posted: true, client: {dbView: 'base.ent_v', loaded: true}}]},
         erd:	{}
       }
     },
+    hasRun:	{},
     tabs:	[
       {tag: 'db',  component: 'tabEditor', title: 'Database', help: 'Direct access to database tables'},
       {tag: 'ent', component: 'entEditor', title: 'Users', help: 'View and edit database user entities'},
@@ -42,11 +45,29 @@ new Vue(Object.assign({el: '#app', template: AppTemplate}, {
     ],
   }},
   computed: {
-    currentComp: function() {
-      return this.tabs.find(e=>(e.tag == this.state.curTab)).component
+    curTab() {
+      return this.state.curTab || 'users'
+    },
+    views() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.view
+        return ac
+      }, {})
+    },
+    components() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.component || 'wylib-launch'
+        return ac
+      }, {})
     },
   },
-//  beforeMount: function() {
-//    this.db = {windows: [{posted: true, client: {dbView: 'wm.table_pub', loaded: true}}]}
-//  },
-}))
+  mounted: function () {				//console.log("Admin init:", this.curTab)
+    if (this.curTab)
+      this.$nextTick(()=>{this.$set(this.hasRun, this.curTab, true)})
+  },
+  watch: {
+    'state.curTab': function(newVal, oldVal) {		//console.log("Watched curTab:", newVal)
+      this.$set(this.hasRun, this.curTab, true)
+    }
+  },
+})
